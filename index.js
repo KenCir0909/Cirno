51922bed1ab064e02bad8c1279dddea34a605321
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-02-17 23:02:13 +0900

    色々

--- index.js
+++ index.js
@@ -2,37 +2,84 @@ require('dotenv').config();
 const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ ws: { intents: Intents.ALL }});
 
+const allInvites = {}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+
+  client.guilds.cache.forEach(guild => {
+    guild.fetchInvites().then(invites => {
+      allInvites[guild.id] = invites
+    })
+  })
 })
 
 client.on('message', async message => {
+  message.author.createdTimestamp
   if(message.author.bot) return;
-  if (message.content === 'チルノ大好き！') {
-    return message.channel.send('あたいも大好きだよ！ <@'+message.author.id+'>');
+  if (message.content === 'チルノちゃん大好き！') {
+    return message.channel.send(`あたいも大好きだよ！ ${message.author}`);
   }
 })
 
 client.on('guildMemberAdd', member => {
     if(member.guild.id !== '794380572323086358') return;
     if(member.user.bot) return member.roles.add('794410823564918835');
-    client.channels.cache.get('794380572931391511').send(`${member}よおこそ`);
+    let invite, inviteuser;
+    member.guild.fetchInvites().then(invites => {
+      const oldInvites = allInvites[member.guild.id];
+      allInvites[member.guild.id] = invites;
+      invite = invites.find(i => oldInvites.get(i.code).uses < i.uses);
+      inviteuser = invites.find(i => oldInvites.get(i.code).inviter);
+    })
+    client.channels.cache.get('811254308989042789').send(`${member} よおこそ`);
+    client.channels.cache.get('794380572931391511').send(
+      new MessageEmbed()
+      .setTitle('ユーザー参加情報')
+      .addField('User名', member.user.tag)
+      .addField('UserID', member.user.id)
+      .addField('垢作ってから経った日数',  Math.round((Date.now() - member.user.createdAt) / 86400000))
+      .addField('使用した招待コード', invite)
+      .addField('招待した人', inviteuser)
+      .setColor('RANDOM')
+      .setTimestamp()
+    );
 })
 
 client.on('guildMemberRemove', member => {
     if(member.guild.id !== '794380572323086358') return;
     if(member.user.bot) return;
-    client.channels.cache.get('794380572931391511').send(`${member}さよなら`);
+    client.channels.cache.get('811254308989042789').send(`${member.user.tag} さよなら`);
+    client.channels.cache.get('794380572931391511').send(
+      new MessageEmbed()
+      .setTitle('ユーザー退出情報')
+      .addField('User名', member.user.tag)
+      .addField('UserID', member.user.id)
+      .addField('垢作ってから経った日数',  Math.round((Date.now() - member.user.createdAt) / 86400000))
+      .addField('参加してた日数', Math.round((Date.now() - member.joinedAt) / 86400000))
+      .setColor('RANDOM')
+      .setTimestamp()
+    )
 })
 
-process.on('unhandledRejection', error => {
+client.on('guildBanAdd', (guild, user) => {
+  if(guild.id !== '794380572323086358') return;
+  client.channels.cache.get('794380572931391511').send(`${user.tag}がBanされました`);
+})
+
+client.on('guildBanRemove', (guild, user) => {
+  if(guild.id !== '794380572323086358') return;
+  client.channels.cache.get('794380572931391511').send(`${user.tag}のBanが解除されました`);
+})
+
+process.on('unhandledRejection', (reason, promise) => {
   client.users.cache.get('714455926970777602').send(
     new MessageEmbed()
-    .setDescription('エラー内容\n```'+error+'```')
+    .setDescription(`エラー内容: ${reason}\n拒否されたプロミス: ${promise}`)
     .setColor('RANDOM')
     .setTimestamp()
   );
-  console.error(`[ERROR!]\n${error}`);
+  console.error(`エラー内容: ${reason}\n拒否されたプロミス: ${promise}`);
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login();
