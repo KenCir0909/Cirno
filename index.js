e19096d1cfc2508b6489647eceef51f5a8750fe0
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-24 18:30:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 require('dotenv').config();
 const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ ws: { intents: Intents.ALL } });
+const all_response = require('./dat/all_response.json');
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('message', async message => {
-  if (message.author.bot) return;
-  if (message.content === 'チルノちゃん大好き！') {
-    return message.channel.send(`あたいも大好きだよ！ ${message.author}`);
+  if (message.system || message.author.bot || !message.content.startsWith('チルノちゃん')) return;
+
+  message.channel.startTyping();
+
+  const response = all_response.find(response => response.reply && response.content.startsWith(message.content));
+
+  if (!response) {
+    sleep(2000).then(() => {
+      message.reply('どうかした？');
+      return message.channel.stopTyping();
+    });
   }
-});
+
+  sleep(Math.ceil(response.reply.length / 2) * 1000).then(() => {
+    message.channel.send(response.reply.replace('{username}', `${message.author.username}くん`));
+    message.channel.stopTyping();
+  });
+})
 
 process.on('unhandledRejection', (reason, promise) => {
   console.error(reason);
